958cdefcaf753a26f1be16c4ceb5b4cafdbdd243
Alexandr Maslov <alexandr.maslov@hm.com>
2022-02-07 08:43:36 +0200

    Bck

--- init.lua
+++ init.lua
@@ -3,18 +3,6 @@ require('keymappings')
 require('plugins')
 require('nvim-cmp')
 
-vim.cmd([[
-  let g:black_linelength = 120
-]])
-
-vim.cmd([[
-  runtime settings-autocomple.vim
-  runtime settings-lsp.vim
-  runtime settings-nvim-cmp.vim
-  runtime settings-lightline.vim
-  runtime settings-ultisnips.vim
-]])
-
 -- Important have it at the end.
 -- https://neovim.io/doc/user/provider.html
 vim.cmd([[
---
+++ lua/settings.lua
@@ -0,0 +1,13 @@
+-- Plugins settings
+vim.cmd([[
+  let g:black_linelength = 120
+]])
+
+vim.cmd([[
+  runtime settings-autocomple.vim
+  runtime settings-lsp.vim
+  runtime settings-nvim-cmp.vim
+  runtime settings-lightline.vim
+  runtime settings-ultisnips.vim
+]])
+
