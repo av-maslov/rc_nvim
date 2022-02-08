7588e24b4d3b99162983cbadbe81041ef4b8c354
Alexandr Maslov <alexandr.maslov@hm.com>
2022-02-08 14:20:27 +0200

    bck

--- lua/basics.lua
+++ lua/basics.lua
@@ -12,15 +12,15 @@ vim.o.wrap = true
 vim.o.linebreak=true
 vim.o.relativenumber=true
 vim.o.number=true
+vim.o.cursorline = false
 
-local set = vim.opt
-vim.o.cursorline = true
+-- local set = vim.opt
 
 -- Set the behavior of tab
-set.expandtab = true
-set.tabstop = 2
-set.softtabstop = 2
-set.shiftwidth = 2
+vim.opt.expandtab = true
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+vim.opt.shiftwidth = 2
 
 -- vim.o.colorcolumn = '80'
 
